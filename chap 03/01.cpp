64f11cd36010a9faad829f5e44ad02948f6c5915
wlyCrystal <67789869+wlyCrystal@users.noreply.github.com>
2024-04-10 02:31:56 +0100

    first commit

---
+++ chap 03/01.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    const int SCALER = 12;
+    int inch_height;
+    cout << "Enter your height in integer inches: __\b\b";
+    cin >> inch_height;
+    cout << "Your height is " << inch_height/SCALER << " feet " << inch_height%SCALER << " inch." << endl;
+    return 0;
+}
